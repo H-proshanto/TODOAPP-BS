0408a87340c244075ad030ba22db09569e8d0c45
Proshanto Lal <proshantolal3@gmail.com>
2022-10-24 19:14:24 +0600

    Adds Formik form in TodoForm

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
-export const InputField = ({ view, text, setter, placeholder }) => {
+export const InputField = ({ view, text, setter, placeholder, onBlur }) => {
   return (
     <>
       <View style={styles.inputContainer}>
@@ -11,6 +11,7 @@ export const InputField = ({ view, text, setter, placeholder }) => {
           placeholder={view === 'read' ? '' : `Enter ${placeholder} Here`}
           onChangeText={setter}
           editable={view === 'read' ? false : true}
+          onBlur={onBlur}
           value={text}
         />
       </View>
--- src/screens/TodoForm.js
+++ src/screens/TodoForm.js
@@ -2,32 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { InputField } from '../components/InputField';
 import { setErrorMessage } from '../features/error';
 import { ReadOnlyViewBtns } from '../components/ReadOnlyViewBtns';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { StyleSheet, View, ScrollView, Text, Alert } from 'react-native';
+import { Formik } from 'formik';
 
 export const TodoForm = ({ navigation, route }) => {
   const errorMessage = useSelector(state => state.error.value);
   const requestError = useSelector(state => state.todo.error);
-  const dispatch = useDispatch();
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
   const todo = route.params?.todo;
   const taskId = todo?.id;
   const view = route.params?.view;
   const status = todo?.is_completed;
-
-  useEffect(() => {
-    if (view === 'read' || view === 'update') {
-      setTitle(todo?.title);
-      setDescription(todo?.description);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!title.length !== 0) {
-      dispatch(setErrorMessage(''));
-    }
-  }, [title]);
 
   useEffect(() => {
     if (requestError.length) {
@@ -39,38 +24,59 @@ export const TodoForm = ({ navigation, route }) => {
     }
   }, [requestError]);
 
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.title) {
+      errors.title = 'Title is required';
+    }
+
+    return errors;
+  }
+
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <InputField
-          view={view}
-          text={title}
-          setter={setTitle}
-          placeholder="Title"
-        />
-        {errorMessage !== '' ? (
-          <Text style={styles.errorMessage}>{errorMessage}</Text>
-        ) : (
-          <></>
-        )}
-        <InputField
-          view={view}
-          text={description}
-          setter={setDescription}
-          placeholder="Description"
-        />
-      </ScrollView>
-      <ScrollView keyboardShouldPersistTaps="always">
-        <ReadOnlyViewBtns
-          view={view}
-          status={status}
-          navigation={navigation}
-          taskId={taskId}
-          title={title}
-          description={description}
-        />
-      </ScrollView>
-    </View>
+    <Formik
+      initialValues={{ title: `${todo?.title || ''}`, description: '' }}
+      validate={validate}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+        <View style={styles.container}>
+          <ScrollView>
+            <InputField
+              view={view}
+              text={values.title}
+              setter={handleChange('title')}
+              onBlur={handleBlur('title')}
+              placeholder="Title"
+            />
+            {errors.title ? (
+              <Text style={styles.errorMessage}>{errors.title}</Text>
+            ) : (
+              <></>
+            )}
+            <InputField
+              view={view}
+              text={values.description}
+              setter={handleChange('description')}
+              onBlur={handleBlur('description')}
+              placeholder="Description"
+            />
+          </ScrollView>
+          <ScrollView>
+
+            <ReadOnlyViewBtns
+              view={view}
+              status={status}
+              navigation={navigation}
+              taskId={taskId}
+              title={values.title}
+              description={values.description}
+            />
+          </ScrollView>
+        </View>
+      )}
+
+    </Formik>
   );
 };
 
